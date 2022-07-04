284bfd8a9bad12ed20d9d3bd5cb4eedc4c069932
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-04 07:54:18 +0200

    Update ex4.cpp

--- chapter5/ex4.cpp
+++ chapter5/ex4.cpp
@@ -8,6 +8,14 @@ void exchange(int* n1, int* n2){
     *n2 = n3;
 }
 
+void exchange(int& n1, int& n2){
+
+    int n3 = n1;
+    n1 = n2;
+    n2 = n3;
+}
+
+
 int main() {
 
 
@@ -16,6 +24,8 @@ int a = 1;
 int b = 2;
 
 exchange(&a, &b);
+std::cout << a << " " << b << std::endl;
+exchange(a, b);
 
 std::cout << a << " " << b << std::endl;
 }
