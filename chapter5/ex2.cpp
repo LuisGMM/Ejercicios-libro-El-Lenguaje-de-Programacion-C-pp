66c1b80c498da4ecbcedd74418c1f214c26425f1
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-04 07:30:43 +0200

    Update ex2.cpp

--- chapter5/ex2.cpp
+++ chapter5/ex2.cpp
@@ -3,14 +3,11 @@
 
 int main() {
 
-char* p;
-int a[1];
-int (& ra)[1] = a;
-char b[10];
-char* pb;
-char** pp;
-extern const int ec;
-int* pec;
-const int* pecc;
+typedef  unsigned char uc;
+typedef const unsigned char cuc;
+typedef int* pi;
+typedef char** ppc;
+typedef char* pc[];
+typedef int** ppi[];
 
 }
