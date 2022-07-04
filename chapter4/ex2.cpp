6219f6d33ecdffe9eac06cc0c052b92b92716d24
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-04 07:09:17 +0200

    Create ex2.cpp

---
+++ chapter4/ex2.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <string>
+
+using std::string;
+
+int main(){
+    char ch = 'a';
+    string c = "hello world";
+    int cuenta;
+    cuenta = 0;
+    string novoyahacermas = "ni de cogna";
+}
