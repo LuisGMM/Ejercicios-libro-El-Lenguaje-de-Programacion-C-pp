5e539fee00c88a14f08044503f0374a581137ff1
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-26 16:59:48 +0200

    Update ex13.cpp

--- chapter5/ex13.cpp
+++ chapter5/ex13.cpp
@@ -1,22 +1,43 @@
-
+# include <iostream>
 
 struct Fecha {
     short dia;
     short mes;
     int agno;
 
-    Fecha(short& dia_, short& mes_, int& agno_): dia(dia_), mes(mes), agno(agno_) {};
+    Fecha(short dia_, short mes_, int agno_): dia(dia_), mes(mes), agno(agno_) {};
 };
 
 
-Fecha makeFecha(short dia, short mes, int agno) {
+Fecha makeFecha(short& dia, short& mes, int& agno) {
 
-    return Fecha(dia, mes, agno); 
+    Fecha fecha = {dia, mes, agno};
+    return fecha;
+}
+
+void printFecha(Fecha& fecha) {
+
+    std::cout << "Fecha: " << fecha.dia << ", " << fecha.mes << ", " << fecha.agno << std::endl;
+}
+
+Fecha readFecha() {
+
+    short dia, mes;
+    int agno;
+
+    std::cin >> dia >> mes >> agno;
+
+    Fecha fecha = {dia, mes, agno};
+    return fecha;
 }
 
 
 int main() {
 
-    makeFecha(0, 0, 0);
+    Fecha fecha = Fecha(0, 0, 0);
+
+    fecha = readFecha();
+
+    printFecha(fecha);
 }
 
