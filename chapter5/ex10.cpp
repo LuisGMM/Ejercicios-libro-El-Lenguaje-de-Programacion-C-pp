93c35f4147179449193418e2370ed28fb32b908b
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-26 17:45:00 +0200

    Update ex10.cpp

--- chapter5/ex10.cpp
+++ chapter5/ex10.cpp
@@ -6,3 +6,10 @@ std::string months[] = {"January", "February", "March", "April", "May",
                  "June", "July", "August", "September", "October",
                  "November", "December"};
 
+
+void looper(std::string array[]) {
+
+    for (short i = 0; i < 12; i++) {
+        std::cout << array[i] << std::endl;
+    }
+}
