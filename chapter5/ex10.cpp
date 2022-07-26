4a363d6559436409f3f06b57e980f28e43d82299
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-26 17:44:52 +0200

    Update ex10.cpp

--- chapter5/ex10.cpp
+++ chapter5/ex10.cpp
@@ -2,3 +2,7 @@
 # include <iostream>
 # include <string>
 
+std::string months[] = {"January", "February", "March", "April", "May",
+                 "June", "July", "August", "September", "October",
+                 "November", "December"};
+
