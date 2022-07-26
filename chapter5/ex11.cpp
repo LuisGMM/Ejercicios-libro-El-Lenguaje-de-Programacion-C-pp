88110397d513a4d86adffc812a4ee36ca30095ac
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-26 17:45:23 +0200

    Update ex11.cpp

--- chapter5/ex11.cpp
+++ chapter5/ex11.cpp
@@ -11,3 +11,19 @@ vector<string> readInput() {
     string word;
     vector<string> words;
 
+    while (true) {
+        getline(cin, word);
+
+        if (word == "Salir") break;
+
+        // checking if word is in words
+        if (find(words.begin(), words.end(), word) == words.end()) words.push_back(word);
+
+        // Alternative way 
+        // bool found = false;
+        // for (short i = 0; i < words.size(); i++) if (words[i] == word) found = true;
+
+        // if (!found) words.push_back(word);
+
+    }
+
